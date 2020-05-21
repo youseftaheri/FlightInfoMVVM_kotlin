a48b42d0e76b5cdbd66823324f398f71c2a8671a
youseftaheri <65353579+youseftaheri@users.noreply.github.com>
2020-05-21 18:31:50 +0430

    Delete LocalUtils.kt

--- app/src/main/java/com/yousef/mvvmflightinfo/utils/LocalUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.yousef.mvvmflightinfo.utils
-
-object LocalUtils {
-
-    val client_id = "ssqkem7uzmwagnnc3kmy6hwe"
-    val client_secret = "eq69KQan4K"
-    val google_maps_key = "AIzaSyCEBr3iJewO7jOIpVLxRVgf6NZ7yUwwvzo"
-}
